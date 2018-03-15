1fa20e5a9fe7407e697f9b74b118daedfe236ce5
Michał <michalstroz@gmail.com>
2018-03-15 23:29:45 +0100

    creating new book functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,30 @@
 class BooksController < ApplicationController
+  before_action :provide_book, only: [:show, :destroy, :edit, :update]
+
   def index
     @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book), notice: "The book has been successfully added"
+  end
+
+  def show
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :publishing_house, :image)
+  end
+
+  def provide_book
+    @book = Book.find(params[:id])
   end
 end
