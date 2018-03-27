03b3fbd06aaad372dfba04eada0ed9a69095a501
Michał <michalstroz@gmail.com>
2018-03-27 21:19:08 +0200

    creating the possibility of book evaluation

--- app/assets/javascripts/stars.js
+++ app/assets/javascripts/stars.js
@@ -5,13 +5,14 @@ function setRate(event){
   putRateIntoField(elementNumber);
   document.getElementById('rating-form').submit();
 }
+
 function putRateIntoField(elNum){
   var hideField = document.getElementById('rate')
   hideField.setAttribute('value', elNum);
 }
 
 window.addEventListener('load', function(){
-  var stars = document.querySelectorAll('.fa.fa-star');
+  var stars = document.querySelectorAll('.star');
   for(var i = 0; i < stars.length; i++){
     stars[i].addEventListener('click', setRate);
   }
