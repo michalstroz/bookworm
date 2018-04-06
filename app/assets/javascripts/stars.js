8a556e7e7e449ec314785085b8001cc596c8a7db
Michał <michalstroz@gmail.com>
2018-04-06 02:05:23 +0200

    create dynamically highlighted stars

--- app/assets/javascripts/stars.js
+++ app/assets/javascripts/stars.js
@@ -1,9 +1,21 @@
+window.addEventListener('load', function(){
+  var stars = document.querySelectorAll('.fa.fa-star');
+  for(var i = 0; i < stars.length; i++){
+    stars[i].addEventListener('click', setRate);
+  }
+  setStarsRating();
+});
+
 function setRate(event){
   var elementId = event.target.id;
   var elementNumber = elementId.match(/\d/g).join('');
   elementNumber = parseInt(elementNumber);
   putRateIntoField(elementNumber);
   document.getElementById('rating-form').submit();
+  var starsRate = document.getElementById('stars_rate');
+  starsRate.className = starsRate.className.replace('stars', 'stars-none');
+  var averageRate = document.getElementById('average-rate');
+  averageRate.className = averageRate.className.replace('stars-none', 'stars-rating');
 }
 
 function putRateIntoField(elNum){
@@ -11,9 +23,12 @@ function putRateIntoField(elNum){
   hideField.setAttribute('value', elNum);
 }
 
-window.addEventListener('load', function(){
-  var stars = document.querySelectorAll('.fa.fa-star');
-  for(var i = 0; i < stars.length; i++){
-    stars[i].addEventListener('click', setRate);
+function setStarsRating(){
+  var averageRating = document.getElementById('average-rating').innerHTML;
+  if (typeof(averageRating) != 'undefined' && averageRating != null) {
+    averageRating = parseFloat(averageRating);
+    var percentage = averageRating * 10.0;
+    var highlightedStars = document.getElementById('highlighted-stars');
+    highlightedStars.style.width = percentage + '%';
   }
-});
+}
