3b9a6c47aa1a0ceebfcb93c2c0cc9d6ab398fa7d
Michał <michalstroz@gmail.com>
2018-04-11 00:31:19 +0200

    routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :books do
     put :rating, on: :member
   end
+
+  get 'top-10-books', to: 'books#best_books'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
