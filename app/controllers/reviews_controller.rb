8dac735bff45650188c5f42e1203ff2dd9d3b172
Michał <michalstroz@gmail.com>
2018-04-13 00:27:35 +0200

    creating review_params and provide_book methods in reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  before_action :provide_book, only: [:create, :destroy]
+
+  def create
+    @review = Review.new(review_params.merge(book: @book))
+    if @review.safe
+      redirect_to book_path(@book)
+    else
+      render 'books/show'
+    end
+  end
+
+  private
+
+  def provide_book
+    @book = Book.find(params[:book_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:name, :contents, :book_id)
+  end
+
 end
