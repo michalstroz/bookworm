b6924df5900c236048ca0934aaba775824cfacd2
Michał <michalstroz@gmail.com>
2018-04-16 00:27:54 +0200

    add user reference to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,7 @@ class Book < ApplicationRecord
   validates :publishing_house, presence: true
   validates :image, attachment_presence: true
 
+  belongs_to :user
   has_many :reviews, dependent: :destroy
 
   has_attached_file :image, default_url: "/images/:style/missing.png"
